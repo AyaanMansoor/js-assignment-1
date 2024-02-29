9ab65d803d2a60e9c4716aaed7901792179b88a3
AyaanMansoor <154600399+AyaanMansoor@users.noreply.github.com>
2024-02-29 21:25:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// Q2
+
+// alert("Error! please enter a valid password.")
+// Q3
+// alert("Welcome To JS Land.\n Happy Coding")
+// Q4
+ //alert("WelcomeTo JS Land...")
+// alert("Happy Coding!")
+//Q5
+//console.log("Hello i can run JS throughmy web browser's console")
+ 
+
+//WITH SWAL
+//Q2
+ //swal("Error! please enter a valid password.")
+ // Q3
+// swal("Welcome To JS Land.\n Happy Coding")
+// Q4
+ //swal("Welcome To JS Land...")
+ //swal("Happy Coding!")
+ 
